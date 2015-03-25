d9bc7038b3a6cb822e3193d8942a13792e8d4c50
lunim <lasers@gmail.com>
2015-03-24 21:25:37 -0700

    Initial Commit

---
+++ HelloConsole/HelloConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var message = new List<String>();
+
+            message.Add("Hello");
+            message.Add("Console!");
+
+            foreach (var entry in message)
+            {
+                Console.WriteLine(entry);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
